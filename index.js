3d6b99475506177f5fa9927525e54ea8d8adf9dc
shyam <yash.shyam007@gmail.com>
2023-02-16 11:58:35 +0000

    Design congrats card

---
+++ index.js
@@ -0,0 +1,15 @@
+const element = (
+  <div className="backContainer">
+    <div>
+      <h1 className="main">Congratulations</h1>
+    </div>
+    <div className="subContainer">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1 className="head">Kiran V</h1>
+      <p>Vishnu Institute of Computer Education and Technology,Bhimavaram</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
